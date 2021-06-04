4b69d8e6a4121032949f1d5d6dcf4a97317c3c5f
Sandesh <sandeshdr36@gmail.com>
2021-06-04 07:59:47 -0700

    This is My First CRUD Project Using Django

---
+++ enroll/forms.py
@@ -0,0 +1,10 @@
+from django.core import validators
+from django import forms
+from .models import User
+
+class Student(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['name', 'email', 'password']
+        labels ={'name':'Enter Your Name', 'email': 'Enter Your Email', 'password': 'Enter Your Password'}
+        widgets = {'name': forms.TextInput(attrs={'class': 'form-control'}),'email': forms.EmailInput(attrs={'class': 'form-control'}),'password': forms.PasswordInput(attrs={'class': 'form-control'}),  }
---
+++ enroll/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render,HttpResponseRedirect
+from .forms import Student
+from .models import User
+
+# Create your views here
+# this function is to add item and show all items
+def show(request):
+    if request.method == 'POST':
+        fm = Student(request.POST)
+        if fm.is_valid():
+            nm = fm.cleaned_data['name']
+            em = fm.cleaned_data['email']
+            pw = fm.cleaned_data['password']
+            reg = User(name=nm, email=em, password=pw)
+            reg.save()
+            fm = Student()
+    else:
+        fm = Student()
+    stu = User.objects.all()
+    return render(request, 'enroll/forms.html', {'form': fm, 'stud':stu})
+
+def deletedata(request, id):
+    if request.method == 'POST':
+        pi = User.objects.get(pk=id)
+        pi.delete()
+        return HttpResponseRedirect('/')
+
+def edit(request, id):
+    if request.method == 'POST':
+        pi = User.objects.get(pk=id)
+        fm = Student(request.POST, instance=pi)
+        if fm.is_valid():
+            fm.save()
+            fm = Student()
+    else:
+        pi = User.objects.get(pk=id)
+        fm = Student(instance=pi)
+    return render(request, 'enroll/edit.html',{'form': fm, 'id': id})
